cc1ab7463ee1dbe493e2e399d4a476ea6858cf36
Shahir Kazi <shahirmuktasid22@gmail.com>
2026-02-26 08:57:51 +0300

    OpenGraph support

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,8 +25,22 @@
 
 <svelte:head>
     <link rel="icon" href="/favicon.png" />
-    
-    <title>{pageTitle} | My Site</title>
+    <title>{pageTitle} | Shahir Kazi</title>
+
+    <meta name="description" content="Software engineering portfolio of Shahir Kazi. Explore my projects, skills, and development experience." />
+    <meta name="author" content="Shahir Kazi" />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content={page.url.href} />
+    <meta property="og:title" content="{pageTitle} | Shahir Kazi" />
+    <meta property="og:description" content="Software engineering portfolio of Shahir Kazi. Explore my projects, skills, and development experience." />
+    <meta property="og:image" content="https://shahirkazi.com/thumbnail.jpg" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:url" content={page.url.href} />
+    <meta name="twitter:title" content="{pageTitle} | Shahir Kazi" />
+    <meta name="twitter:description" content="Software engineering portfolio of Shahir Kazi. Explore my projects, skills, and development experience." />
+    <meta name="twitter:image" content="https://shahirkazi.com/thumbnail.jpg" />
 </svelte:head>
 
 <Navbar />
